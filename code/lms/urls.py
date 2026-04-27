e2096df702accdf0c428b12dc73f70aac1f1d46f
Pasyah Vegananda <32693282+PVegananda@users.noreply.github.com>
2026-04-27 20:09:26 +0700

    step 1.4 tambah url

--- code/lms/urls.py
+++ code/lms/urls.py
@@ -9,9 +9,11 @@ Routes:
 
 from django.contrib import admin
 from django.urls import path, include
+from courses.apiv1 import apiv1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('silk/', include('silk.urls', namespace='silk')),
+    path('api/v1/', apiv1.urls),
     path('', include('courses.urls')),
 ]
